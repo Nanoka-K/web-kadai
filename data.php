c1475f5cb07f7feb9a7672b51f958547bc5dd3c7
SHIBUYA Yuuya <jt-202513@kurihara.ac.jp>
2026-05-28 11:14:19 +0900

    内容を追加しました

---
+++ data.php
@@ -0,0 +1,5 @@
+ <!-- 読み込まれる側のファイル -->
+<?php
+    $people [] = ['name' => '佐藤','blood' => 'A'];
+    $people [] = ['name' => '田中','blood' => 'B'];
+    $people [] = ['name' => '加藤','blood' => 'O'];
---
+++ view.php
@@ -0,0 +1,6 @@
+<!-- 呼び出す側のファイル -->
+ <?php
+    //require_onceのあとは、かっこかスペース
+    //''の中は読み込まれる側のファイルの相対パス
+    require_once 'data.php';
+    var_dump($people);
